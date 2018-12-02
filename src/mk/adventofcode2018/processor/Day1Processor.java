7f25f48b317ef237ef2bc99bfad25b930ae63171
Michelle <michellekoob828@gmail.com>
2018-12-02 00:50:19 -0600

    Added Day1 project

---
+++ src/mk/adventofcode2018/processor/Day1Processor.java
@@ -0,0 +1,41 @@
+package mk.adventofcode2018.processor;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day1Processor {
+	
+	public static final String FILE_NAME = "src/InputFileDay1.txt";
+
+	public void process() throws FileNotFoundException {
+		
+		FileReader fr = new FileReader(FILE_NAME); 
+		BufferedReader bufr = new BufferedReader(fr);
+		
+		int frequency = 0;
+				
+		try {
+			String line = bufr.readLine();
+			
+			while(line != null){
+				frequency = this.updateFrequency(line, frequency);
+				line = bufr.readLine(); 
+			}
+			
+			System.out.println("Frequency " + frequency);
+			bufr.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
+	private int updateFrequency(String frequencyChangeString, int frequency) {
+		int frequencyChange = Integer.parseInt(frequencyChangeString);
+		frequency += frequencyChange;
+		System.out.println("Frequency change " + frequencyChange);
+		return frequency;
+	}
+}
